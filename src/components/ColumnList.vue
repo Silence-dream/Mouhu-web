cf41326b0eaa7dea1b9a68ddb824ad6b041b14ca
ℳ๓₯㎕白月初 <42824008+Silence-dream@users.noreply.github.com>
2020-11-22 16:58:57 +0800

    :lipstick: 文章专栏组件样式修改完毕

--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -1,15 +1,17 @@
 <template>
-  <el-row>
+  <el-row :gutter="30">
     <el-col
       class="column-list-item"
       :span="8"
       v-for="item in list"
       :key="item.id"
     >
-      <img :src="item.avatar" />
-      <h5>{{ item.title }}</h5>
-      <p>{{ item.description }}</p>
-      <a href="javascript:;">进入专栏</a>
+      <div class="box-item">
+        <img :src="item.avatar" />
+        <h5>{{ item.title }}</h5>
+        <p>{{ item.description }}</p>
+        <a href="javascript:;">进入专栏</a>
+      </div>
     </el-col>
   </el-row>
 </template>
@@ -38,6 +40,49 @@ export default defineComponent({
 
 <style scoped lang="scss">
 .column-list-item:nth-child(2n + 1) {
-  background: pink;
+  //background: pink;
+}
+.column-list-item {
+  .box-item {
+    margin: 20px 0;
+    text-align: center;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
+  }
+  img {
+    display: block;
+    margin: 0 auto;
+  }
+  h5 {
+    color: #303133;
+    font-size: 1.25rem;
+  }
+  p {
+    color: #909399;
+    line-height: 20px;
+    height: 60px;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+    word-break: break-all;
+    display: -webkit-box;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
+  a {
+    color: #0d6efd;
+    display: inline-block;
+    font-weight: 400;
+    line-height: 1.5;
+    text-align: center;
+    text-decoration: none;
+    vertical-align: middle;
+    cursor: pointer;
+    user-select: none;
+    background-color: transparent;
+    border: 1px solid #0d6efd;
+    padding: 0.375rem 0.75rem;
+    font-size: 1rem;
+    border-radius: 0.25rem;
+    margin-bottom: 20px;
+  }
 }
 </style>
