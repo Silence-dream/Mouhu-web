807fffb90fce083b609befd3f94bdd63d91858e4
Silence-dream <1428482231@qq.com>
2020-12-11 14:32:08 +0800

    :heavy_plus_sign:  删除dist环境里面的console.log

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+module.exports = {
+  publicPath: "./",
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === "production") {
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true;
+    }
+  }
+};
